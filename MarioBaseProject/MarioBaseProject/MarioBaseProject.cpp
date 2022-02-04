8f813c865dc89e8dd77f13a58a1123e39a053847
Bonita Woralert <67742281+BonitaWoralert@users.noreply.github.com>
2022-02-04 12:04:08 +0000

    Opening a window complete!

--- MarioBaseProject/MarioBaseProject/MarioBaseProject.cpp
+++ MarioBaseProject/MarioBaseProject/MarioBaseProject.cpp
@@ -6,8 +6,62 @@
 #include <SDL_image.h>
 #include <SDL_mixer.h>
 
+#include "constants.h"
+
+
+//Globals 
+SDL_Window* g_window = nullptr;
+
+//Function prototypes
+bool InitSDL();
+void CloseSDL();
+
 int main(int argc, char* args[])
 {
-	std::cout << "IT WORKS!" << std::endl;
+	//Check sdl setup correctly
+	if (InitSDL())
+	{
+		SDL_Delay(5000);
+	}
+
+	CloseSDL();
+
 	return 0;
+}
+
+bool InitSDL()
+{
+	//Setup SDL
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		std::cout << "SDL did not initialise. Error: " << SDL_GetError();
+		return false;
+	}
+	else
+	{
+		//setup passed so create window
+		g_window = SDL_CreateWindow("Games Engine Creation",
+			SDL_WINDOWPOS_UNDEFINED,
+			SDL_WINDOWPOS_UNDEFINED,
+			SCREEN_WIDTH,
+			SCREEN_HEIGHT,
+			SDL_WINDOW_SHOWN);
+		//did window get created?
+		if (g_window == nullptr)
+		{
+			//window failed
+			std::cout << "Window was not created. Error: " << SDL_GetError();
+			return false;
+		}
+	}
+}
+void CloseSDL()
+{
+	//release the window
+	SDL_DestroyWindow(g_window);
+	g_window = nullptr;
+
+	//quit SDL subsystems
+	IMG_Quit();
+	SDL_Quit();
 }
---
+++ MarioBaseProject/MarioBaseProject/constants.h
@@ -0,0 +1,4 @@
+#pragma once
+//Screen dimensions
+#define SCREEN_WIDTH 512
+#define SCREEN_HEIGHT 416
