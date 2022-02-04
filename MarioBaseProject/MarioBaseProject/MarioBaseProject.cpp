3859320465519ac48967b0d1c280568465d5f25b
Bonita Woralert <67742281+BonitaWoralert@users.noreply.github.com>
2022-02-04 11:39:34 +0000

    SDL base setup

---
+++ MarioBaseProject/MarioBaseProject/MarioBaseProject.cpp
@@ -0,0 +1,13 @@
+// MarioBaseProject.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_mixer.h>
+
+int main(int argc, char* args[])
+{
+	std::cout << "IT WORKS!" << std::endl;
+	return 0;
+}
