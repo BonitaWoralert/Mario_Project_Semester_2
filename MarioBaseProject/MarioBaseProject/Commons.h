47348a669c046cc95b5d7459696e7dff431e65c1
Bonita Woralert <67742281+BonitaWoralert@users.noreply.github.com>
2022-02-11 12:20:59 +0000

    texture 2d class created

---
+++ MarioBaseProject/MarioBaseProject/Commons.h
@@ -0,0 +1,19 @@
+#pragma once
+
+struct Vector2D
+{
+	float x;
+	float y;
+
+	Vector2D()
+	{
+		x = 0.0f;
+		y = 0.0f;
+	}
+
+	Vector2D(float initial_x, float initial_y)
+	{
+		x = initial_x;
+		y = initial_y;
+	}
+};
