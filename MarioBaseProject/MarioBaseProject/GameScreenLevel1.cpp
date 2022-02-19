f073186d083ce5d39ad4d4300fd291faf4f76d0c
Bonita Woralert <67742281+BonitaWoralert@users.noreply.github.com>
2022-02-19 00:18:49 +0000

    Game screen + lvl1 class created

---
+++ MarioBaseProject/MarioBaseProject/GameScreenLevel1.cpp
@@ -0,0 +1,1 @@
+#include "GameScreenLevel1.h"
---
+++ MarioBaseProject/MarioBaseProject/GameScreenLevel1.h
@@ -0,0 +1,11 @@
+#pragma once
+#ifndef _GAMESCREENLEVEL1_H
+#include "GameScreen.h"
+#include "Commons.h"
+
+class GameScreenLevel1
+{
+};
+
+
+#endif // !_GAMESCREENLEVEL1_H
